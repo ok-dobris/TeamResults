621bd22ef6c3af53f9da5262fe5750aca24e5da1
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2023-11-06 09:40:11 +0100

    Fix: make Result.status optional

--- src/main/scala/com/github/okdobris/results/model/Result.scala
+++ src/main/scala/com/github/okdobris/results/model/Result.scala
@@ -1,12 +1,10 @@
 package com.github.okdobris.results.model
 
-import java.time._
-
 case class Result(
   startTime: String, // TODO: parse as ZonedDateTime
   finishTime: String,
   time: Int,
   position: Option[Int],
-  status: String,
+  status: Option[String],
   splitTime: List[SplitTime]
 )
